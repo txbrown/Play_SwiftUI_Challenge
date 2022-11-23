87c00aec0a1a31ea7748ed22cb1f17ab79ba1393
Ricardo <ricardo@yonkolevel.com>
2022-11-23 17:09:33 +0000

    Initial Commit

---
+++ Play_SwiftUI_Challenge/Play_SwiftUI_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Play_SwiftUI_ChallengeApp.swift
+//  Play_SwiftUI_Challenge
+//
+//  Created by Ricardo Abreu on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Play_SwiftUI_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Play_SwiftUI_ChallengeUITests/Play_SwiftUI_ChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Play_SwiftUI_ChallengeUITestsLaunchTests.swift
+//  Play_SwiftUI_ChallengeUITests
+//
+//  Created by Ricardo Abreu on 23/11/2022.
+//
+
+import XCTest
+
+final class Play_SwiftUI_ChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
